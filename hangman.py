40cdc14a82975603acd7456fd87e78988974b9d3
umi <asiessoyeltier1@gmail.com>
2021-06-25 08:50:41 -0500

    Already to update, my new model

--- hangman.py
+++ hangman.py
@@ -1,180 +1,70 @@
-import os
+""" hangaman class with all methods"""
+
 from random import randint, randrange
 from unidecode import unidecode
+from console import ascii_art
 
-def get_word():
-
-    """ Gets the random word 
-    """
-
-    with open("words/data.txt","r", encoding="utf-8") as f:
-
-        w_list = [word for word in f if word != '\n']
-        random_word = unidecode(w_list[randint(1,len(w_list))])
-        word_l = [ {letter.upper(): "__" } for letter in random_word if letter != "\n"]        
-
-    return word_l
+class Hangman:
 
 
-def confirm(letter: str, word: list):
+    def __init__(self) -> None:
 
-    """ Confirms the existence of the letter in the list 
-    """
-    i_ = 0
+        """ Define the principal status of the hangman
+        """
 
-    for i in range(len(word)):
-        try:
-            if letter in word[i].keys():
-                i_ += 1  
-                word.insert(i, str(letter))
-                word.remove({letter: "__"})
-            
+        # Define the word
+        self._word, self.incognite = self._get_word()
+
+        #The body for the hangman
+        self.body = ascii_art.hangman_body()
+
+        self.status = self.body[0]
+
+
+    def _get_word(self) -> tuple:
+
+        """ Gets the random word , and convert it in a dict.
+        """
+
+        with open("console/assets/data.txt","r", encoding="utf-8") as f:
+
+            w_list = [word for word in f if word != "\n"]
+            random_word = unidecode(w_list[randint(1,len(w_list))])
+            self._word = [letter.upper() for letter in random_word if letter != "\n"]  
+            self.incognite = ["__" for i in self._word ]
+
+            return (self._word, self.incognite)
+
+
+    def confirm(self, letter: str) -> tuple :
+        
+        """ Confirms the existence of the letter in the list. If the letter is correct, the incognite list gets replace the letter.
+        """
+
+
+        if self._word == self.incognite:
+
+            return  "Lo lograste, completaste la palabra."
+
+        else:
+
+            for i in range(len(self._word)):
+
+                if letter == self._word[i]:
+
+                    self.incognite.remove(self.incognite[i])
+                    self.incognite.insert(i,letter)
+                    return (True, "Le atinaste Muy bien!", self.incognite)
+                else:
+                    continue
+
             else:
-                continue
+
+                return (False, "Lo lamento no has atinado")
+
+
+
+
+
+
     
-        except AttributeError:
-            return True
-
-    if i_ > 0 :
-        return word
-    else:
-        return print(f" {letter} no está dentro de la palabra")
-
-
-
-def hangman_ascii():
-
-    """ Store a constant with the hangman parts
-    """
-
-    HANGMANPICS = ['''
-  +---+
-      |
-      |
-      |
-      |
-      |
-=========''',
-        '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========''']
-
-
-
-def console():
-
-    """ The console game
-    """
-    
-    i = 0
-    random_word = get_word()
-    incognite = "__ "*len(random_word)
-
-    print("##"*65, end="\n")
-    print(f" Adivina la palabra de {len(random_word)} letras de largo.\n")
-    print(incognite)
-
-    user_letter = input("\nEscribe la letra que piensas: ").upper()
-    assert user_letter.isnumeric() == False, "No hay ninguna palabra con números"
-
-    confirm(user_letter, random_word)
-
-    while i == 0:
-
-        for j in random_word:
-            try:
-                incognite = incognite + " " + j.values()
-            except AttributeError:
-                incognite = incognite + " " + j
-
-
-        print("##"*65, end="\n")
-        
-        user_letter = input("\nEscribe la letra que piensas: ").upper()
-        assert user_letter.isnumeric() == False, "No hay ninguna palabra con números"
-
-        print(confirm(user_letter, random_word))
-        
-        
-
-
-def run():
-    """ Start the game with a greeting
-    """
-
-    #Greeting and rules
-
-    print("""
- _                                             
-| |                                            
-| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
-| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
-| | | | (_| | | | | (_| | | | | | | (_| | | | |
-|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-                    __/ |                      
-                   |___/      
-    """)
-    print("==="*40)
-    print("                                                       ¡ BIENVENIDO !                                                   \n")
-
-    print("Las reglas son muy simples, se generará una palabra aleatoria y se te pedirá que adivines letra por letra. Cuando adivines, automáticamente se revelarán las letras en la palabra.")
-
-    #The start part
-
-    print("Okay Empecemos con tu primera palabra")
-    console()
-
-
-def test():
-    pass
-    
-
-if __name__ == '__main__':
-    test()
